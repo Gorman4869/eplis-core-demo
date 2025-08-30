aeb6a37996b00233c6f102ffd0d85f772e42602b
Gorman <838584740@qq.com>
2025-08-30 13:33:32 +0800

    feat: 依赖安装

---
+++ index.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+
+const app = new Koa();
+try {
+  const port = process.env.PORT || 8080;
+  const host = process.env.HOST || "0.0.0.0";
+  app.listen(port, host);
+  console.log(`Server running on http://${host}:${port}`);
+} catch (error) {
+  console.error(error);
+}
